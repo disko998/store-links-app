1a306e3e3aa80b66cb6a6c5bfabb8b6ceb79b317
Stefan <disko998@gmail.com>
2020-09-02 16:40:29 +0200

    google maps ios

--- ios/LinksApp/AppDelegate.m
+++ ios/LinksApp/AppDelegate.m
@@ -13,6 +13,8 @@
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
@@ -28,9 +30,12 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyANEopqKDCaURhLPR5EWaIXL9ekPfa2koM"];
+
   if ([FIRApp defaultApp] == nil) {
     [FIRApp configure];
   }
+
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
